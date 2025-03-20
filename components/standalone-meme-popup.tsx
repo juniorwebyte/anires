f50dfbafcc2bc384bf2c80c08cd6ae3e55c56320
Júnior Alves <39176934+juniorwebyte@users.noreply.github.com>
2025-03-19 21:54:07 -0300

    Delete components directory

--- components/standalone-meme-popup.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-// Este componente foi desativado para evitar popups duplicados
-export default function StandaloneMemePopup() {
-  // Retorna null para não renderizar nada
-  return null
-}
-
