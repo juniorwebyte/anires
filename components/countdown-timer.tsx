e055a33b5948b1b5965d3f386a07b7439e8d1d7e
Júnior Alves <junior@webytebr.com>
2025-03-21 21:55:51 -0300

    Ajustes do cronômetro

--- components/countdown-timer.tsx
+++ components/countdown-timer.tsx
@@ -148,7 +148,10 @@ export default function CountdownTimer() {
         </div>
 
         <div className="text-center">
-          <Button onClick={() => setIsStarted(true)} className="bg-purple-500 text-white px-4 py-2 rounded">
+          <Button onClick={() => {
+            setIsStarted(true)
+            setLaunchDate(new Date(inputDate))
+          }} className="bg-purple-500 text-white px-4 py-2 rounded">
             Iniciar Cronômetro
           </Button>
         </div>
